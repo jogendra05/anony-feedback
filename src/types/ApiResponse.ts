d4840366b2809421bce980ef2eda161cbf5d1381
jogendra05 <jogendrasuthar03@gmail.com>
2025-06-01 17:21:07 +0530

    Db config

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,9 @@
+import { Message } from "@/models/Users";
+
+export interface ApiResponse {
+    success: boolean;
+    message: string;
+    isAcceptingMessages?: boolean;
+    messages: Array<Message> 
+}
+
